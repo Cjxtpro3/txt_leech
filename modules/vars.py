ae1d309f452b403803cc5f3b8d7c7ceaea3bb25e
Cjxtpro3 <152521877+Cjxtpro3@users.noreply.github.com>
2024-04-29 19:34:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "23421759"
+api_hash = "ba8ae5b1f703fc771d1e8425db8752d1"
+bot_token = "6791364552:AAHww0wr2FJ5P0_ikGD3PvYb1yBVX6RdhEc"
